548a9f66ecf09fa44b4efc46de581e53f4b035a4
Isak <din.epost@kth.se>
2026-09-04 11:25:27 +0200

    h

--- Isaks arbetsfil upg 1.py
+++ Isaks arbetsfil upg 1.py
@@ -1,5 +1,6 @@
 import numpy as np
 import matplotlib.pyplot as plt
+
 """Ritar en graf av funktionen och dess derivata uppgift A"""
 L = 1
 
@@ -23,7 +24,9 @@ plt.legend() # öppna rutan
 plt.show() # visa grafen
 
 
-
+"""Uppgift C python program för Fixpunktsmetoden"""
+#fixmetoden lista för differanser
+diff_fixpunkt = [] 
 L = 1
 g = lambda x: (3 * L / 8) * (
     3 * (x / L) ** 2
@@ -33,18 +36,23 @@ g = lambda x: (3 * L / 8) * (
 
 tol = 1e-10
 i, max_iter = 0, 500
-x = 0.45  # 1. Definiera ett startvärde x0
+x = 0.8  # 1. Definiera ett startvärde x0
 
 diff = 1.0  # Sätt ett startvärde för diff så loopen startar
 
 while diff > tol and i < max_iter:
   xn = g(x)  # Beräkna nästa värde
   diff = np.abs(xn - x)  # Beräkna skillnaden innan x uppdateras
+  diff_fixpunkt.append(diff)
   x = xn  # Uppdatera x till det nya värdet
   i += 1  # Öka rätt räknare
-  print(f"försök {i}: x = {x:.16f}, diff = {diff:.2e}") 
+  print(f"försök {i}: x = {x:.10f}, diff = {diff:.2e}") 
   #skriver ut på snnyggt sätt med antal decimaler
 
+
+"""uppgift D Newton metoden"""
+#newtons metod lista för differanser
+diff_newton = [] 
 L = 1
 g = lambda x: (
     (8 * x) / (3 * L)
@@ -61,13 +69,23 @@ gd = lambda x: (
 
 tol = 1e-10
 i, max_iter = 0, 500
-x = 0.4  # 1. Definiera ett startvärde x0
+x = 0.8  # 1. Definiera ett startvärde x0
 diff = 1.0  # Sätt ett startvärde för diff så loopen startar
 
 while diff > tol and i < max_iter:
   xn = x - g(x) / gd(x)  # Beräkna nästa värde
   diff = np.abs(xn - x)  # Beräkna skillnaden INNAN x uppdateras
+  diff_newton.append(diff)
   x = xn  # Uppdatera x till det nya värdet
   i += 1  # Öka rätt räknare
   print(f"försök {i}: x = {x:.10f}, diff = {diff:.2e}")
-  
+
+
+
+"""Uppgift e jämför hur de olika metoderna konvergerar"""
+plt.semilogy(diff_fixpunkt, label="Fixpunktsmetoden") #plottar fixpmetod och döper den
+plt.semilogy(diff_newton, label="Newton-Raphson")# plottar newton och namn
+
+plt.grid(True, which="both") #skapar ett rutnät
+plt.legend() #skapar en lilten ruta i hörnet med namn
+plt.show() #visar plottarna
